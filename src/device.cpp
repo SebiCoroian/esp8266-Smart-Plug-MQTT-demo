72e3685d067e8c26ee926895438c07900625f37f
cezarmathe <cezarmathe@gmail.com>
2018-08-23 17:12:44 +0300

    Refactored device

---
+++ src/device.cpp
@@ -0,0 +1,18 @@
+#include "device.h"
+
+void setRelayState(int relayState)
+{
+    if (relayState == 0) {
+      digitalWrite(relayPin, HIGH);
+      relayState = true;
+      digitalWrite(LED, LOW);
+      Serial.print("Relay ON.");
+    }
+    if (relayState == 1) {
+      digitalWrite(relayPin, LOW);
+      relayState = false;
+      digitalWrite(LED, HIGH);
+      Serial.print("Relay OFF.");
+    }
+    return;
+}
---
+++ src/device.h
@@ -0,0 +1,8 @@
+
+#define LED 2
+#define ON 1
+#define OFF 0
+
+bool relayState = false;
+
+void setRelayState(int relayState);
